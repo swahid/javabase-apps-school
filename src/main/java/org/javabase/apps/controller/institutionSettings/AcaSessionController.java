4fbb298a13a2af3c1b0a57eab20ab8cbdfa9c5ac
rashed21 <rashedunnabi21@gmail.com>
2017-01-17 22:30:50 +0600

    17JAN2017 22:30 PM

--- src/main/java/org/javabase/apps/controller/institutionSettings/AcaSessionController.java
+++ src/main/java/org/javabase/apps/controller/institutionSettings/AcaSessionController.java
@@ -1,7 +1,5 @@
 package org.javabase.apps.controller.institutionSettings;
 
-import org.javabase.apps.service.AcaSessionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/acaSession")
 public class AcaSessionController {
 
-	@Autowired
-	AcaSessionService acaSessionService;
+	/*@Autowired
+	AcaSessionService acaSessionService;*/
 	
 	@RequestMapping(method = RequestMethod.GET)
     public String roomPage() {
