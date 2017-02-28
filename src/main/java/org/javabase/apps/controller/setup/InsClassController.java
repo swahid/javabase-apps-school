22684f64c57630b2ac0c59628ec377b19b7659ad
Saurav Wahid <saurav@medisys.com.sa>
2017-02-28 17:10:11 +0600

    28FEB2017 05:09 PM class Fixed

--- src/main/java/org/javabase/apps/controller/setup/InsClassController.java
+++ src/main/java/org/javabase/apps/controller/setup/InsClassController.java
@@ -9,11 +9,9 @@ import org.javabase.apps.entity.InsClass;
 import org.javabase.apps.service.InsClassService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
